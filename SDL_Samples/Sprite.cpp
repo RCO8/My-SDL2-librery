2a4b932670d186fa80aec0c526c6fc6e80cb1f62
RCO8 <39510436+RCO8@users.noreply.github.com>
2023-09-14 09:06:20 +0900

    Update Sprite.cpp

--- SDL_Samples/Sprite.cpp
+++ SDL_Samples/Sprite.cpp
@@ -38,24 +38,24 @@ void Sprite::SetColorHide(Uint8 r, Uint8 g, Uint8 b)
 
 void Sprite::Drawing()
 {
-	SDL_Rect sprRct;
+	SDL_Rect sprClip;
 	sprRct.x = 0;
 	sprRct.y = 0;
 	sprRct.w = 20;
 	sprRct.h = 80;
 
-	SDL_Rect sprEnd;
+	SDL_Rect sprPos;
 	sprEnd.x = 30;
 	sprEnd.y = 50;
-	sprEnd.w = 30;
-	sprEnd.h = 100;
+	sprEnd.w = sprRct.w;
+	sprEnd.h = sprRct.h;
 
 	/*
 	텍스쳐화된 스프라이트를 그린다
 	두 매개변수 SDL_Rect가 NULL이면 화면 전체 출력
 	지정된 사각형에서 표시될 항목, 실제 좌표에 표시될 항목
 	*/
-	SDL_RenderCopy(sprRenderer, sprTexture, NULL, &sprEnd);
-	//SDL_RenderCopyEx(sprRenderer, sprTexture,nullptr, &sprRct, NULL, nullptr, SDL_FLIP_NONE);
+	SDL_RenderCopy(sprRenderer, sprTexture, &sprClip, &sprPos);
+	//SDL_RenderCopyEx(sprRenderer, sprTexture, nullptr, &sprClip, &sprPos, nullptr, SDL_FLIP_NONE);
 
 }
