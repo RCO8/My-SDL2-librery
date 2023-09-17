90cd5e86a73870607fe960ba3dce075ed49aee5e
RCO8 <39510436+RCO8@users.noreply.github.com>
2023-09-18 01:18:35 +0900

    Add files via upload

--- SDL_Samples/Gaming.cpp
+++ SDL_Samples/Gaming.cpp
@@ -24,6 +24,7 @@ bool Gaming::GameInit()
     }
 
     bgImg = new Sprite(renderer, "goblin.bmp");
+    soundEffect = new Sound("P_MarcoDeath_old.wav");
 
     return true;
 }
@@ -31,13 +32,16 @@ bool Gaming::GameInit()
 void Gaming::CheckKeyPress()
 {
     SDL_Event event;
+
+    //컨트롤러와 조이스틱 입력처리 충돌하지 않도록
     SDL_GameController* myController;   //연결된 컨트롤러와 통신
     SDL_Joystick* myJoystick;           //연결된 조이스틱과 통신
 
     myController = SDL_GameControllerOpen(0);
-    const int controllerMaxAxis = 32768;
+    const int controllerMaxAxis = 32768;    //최대 스틱 데드값
+
     myJoystick = SDL_JoystickOpen(0);
-    const int joystickFixAxis = 256;
+    const int joystickFixAxis = 256;    //스틱 미입력시 256이 지정되버리는것을 방지
     
     while (SDL_PollEvent(&event))
         switch (event.type)
@@ -73,13 +77,15 @@ void Gaming::CheckKeyPress()
                 break;
             case SDL_SCANCODE_F3:       SDL_Log("F3\n");
                 break;
+            case SDL_SCANCODE_F4:       SDL_Log("F4\n");
+                break;
             case SDL_SCANCODE_W:        SDL_Log("W\n");
                 break;
             case SDL_SCANCODE_S:        SDL_Log("S\n");
                 break;
             case SDL_SCANCODE_A:        SDL_Log("A\n");
                 break;
-            case SDL_SCANCODE_D:        SDL_Log("D");
+            case SDL_SCANCODE_D:        SDL_Log("D\n");
                 break;
             case SDL_SCANCODE_ESCAPE:   SDL_Log("Escape\n");
                 break;
@@ -97,8 +103,7 @@ void Gaming::CheckKeyPress()
                 break;
             }
             break;
-        case SDL_MOUSEMOTION:
-            SDL_Log("Mouse : (%d, %d)\n", event.motion.x, event.motion.y);
+        case SDL_MOUSEMOTION: SDL_Log("Mouse : (%d, %d)\n", event.motion.x, event.motion.y);
             break;
         case SDL_MOUSEWHEEL:
             switch (event.wheel.y)
@@ -113,58 +118,88 @@ void Gaming::CheckKeyPress()
         //컨트롤러 입력(콘솔)
         case SDL_CONTROLLERBUTTONDOWN:
             if (myController)
-            {
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_A)) SDL_Log("Button A\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_B)) SDL_Log("Button B\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_X)) SDL_Log("Button X\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_Y)) SDL_Log("Button Y\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_LEFTSHOULDER)) SDL_Log("Left Button\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER)) SDL_Log("Right Button\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_LEFTSTICK)) SDL_Log("Left Stick\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_RIGHTSTICK)) SDL_Log("Right Stick\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_DPAD_UP)) SDL_Log("DPAD UP\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_DPAD_DOWN)) SDL_Log("DPAD DOWN\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_DPAD_LEFT)) SDL_Log("DPAD LEFT\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_DPAD_RIGHT)) SDL_Log("DPAD RIGHT\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_START)) SDL_Log("Start Button\n");
-                if (SDL_GameControllerGetButton(myController, SDL_CONTROLLER_BUTTON_BACK)) SDL_Log("Back Button\n");
-            }
+                switch (event.cbutton.button)
+                {
+                case SDL_CONTROLLER_BUTTON_A:   SDL_Log("Button A\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_B:   SDL_Log("Button B\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_X:   SDL_Log("Button X\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_Y:   SDL_Log("Button Y\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_LEFTSHOULDER:    SDL_Log("Left Button\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER:   SDL_Log("Right Button\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_LEFTSTICK:   SDL_Log("Left Stick\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_RIGHTSTICK:  SDL_Log("Right Stick\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_DPAD_UP:     SDL_Log("DPAD UP\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_DPAD_DOWN:   SDL_Log("DPAD DOWN\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_DPAD_LEFT:   SDL_Log("DPAD LEFT\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:  SDL_Log("DPAD RIGHT\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_START:       SDL_Log("Start Button\n");
+                    break;
+                case SDL_CONTROLLER_BUTTON_BACK:        SDL_Log("Back Button\n");
+                    break;
+                }
             break;
         case SDL_CONTROLLERAXISMOTION:
             if (myController)
-            {
-                //최대 값 : 32768
-                if (SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_LEFTX))
+                switch (event.caxis.axis)
+                {
+                case SDL_CONTROLLER_AXIS_LEFTX:
                     SDL_Log("Left Axis X: %d\n", SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_LEFTX));
-                if (SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_LEFTY))
+                    break;
+                case SDL_CONTROLLER_AXIS_LEFTY:
                     SDL_Log("Left Axis Y: %d\n", SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_LEFTY));
-                if (SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_RIGHTX))
+                    break;
+                case SDL_CONTROLLER_AXIS_RIGHTX:
                     SDL_Log("Right Axis X: %d\n", SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_RIGHTX));
-                if (SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_RIGHTY))
+                    break;
+                case SDL_CONTROLLER_AXIS_RIGHTY:
                     SDL_Log("Right Axis Y: %d\n", SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_RIGHTY));
-                if (SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_TRIGGERLEFT))
+                    break;
+                case SDL_CONTROLLER_AXIS_TRIGGERLEFT:
                     SDL_Log("Left Trigger: %d\n", SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_TRIGGERLEFT));
-                if (SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_TRIGGERRIGHT))
+                    break;
+                case SDL_CONTROLLER_AXIS_TRIGGERRIGHT:
                     SDL_Log("Right Trigger: %d\n", SDL_GameControllerGetAxis(myController, SDL_CONTROLLER_AXIS_TRIGGERRIGHT));
-            }
+                    break;
+                }
             break;
 
         //조이스틱 입력(아케이드)
         case SDL_JOYBUTTONDOWN:
             if (myJoystick)
-            {
                 for (int i = 0; i < 12; i++)
                     if (SDL_JoystickGetButton(myJoystick, i))
                            SDL_Log("Joystick Button %d\n", i);
+            break;
+        case SDL_JOYHATMOTION:
+            if (myJoystick)
+            {
+                if (SDL_JoystickGetHat(myJoystick, 0) == SDL_HAT_UP)        SDL_Log("Hat Up\n");
+                else if (SDL_JoystickGetHat(myJoystick, 0) == SDL_HAT_DOWN) SDL_Log("Hat Down\n");
+                
+                if (SDL_JoystickGetHat(myJoystick, 0) == SDL_HAT_LEFT)      SDL_Log("Hat Left\n");
+                else if (SDL_JoystickGetHat(myJoystick, 0) == SDL_HAT_RIGHT)SDL_Log("Hat Right\n");
             }
             break;
         case SDL_JOYAXISMOTION:
             if (myJoystick)
             {
-                if (SDL_JoystickGetAxis(myJoystick, 0) < -joystickFixAxis) SDL_Log("Joystick Left\n");
-                if (SDL_JoystickGetAxis(myJoystick, 0) > joystickFixAxis) SDL_Log("Joystick Right\n");
-                if (SDL_JoystickGetAxis(myJoystick, 1) < -joystickFixAxis) SDL_Log("Joystick Up\n");
-                if (SDL_JoystickGetAxis(myJoystick, 1) > joystickFixAxis) SDL_Log("Joystick Down\n");
+                if (SDL_JoystickGetAxis(myJoystick, 0) < -joystickFixAxis)   SDL_Log("Joystick Left\n");
+                else if (SDL_JoystickGetAxis(myJoystick, 0) >  joystickFixAxis)   SDL_Log("Joystick Right\n");
+                
+                if (SDL_JoystickGetAxis(myJoystick, 1) < -joystickFixAxis)   SDL_Log("Joystick Up\n");
+                else if (SDL_JoystickGetAxis(myJoystick, 1) >  joystickFixAxis)   SDL_Log("Joystick Down\n");
             }
             break;
         }
@@ -178,7 +213,7 @@ void Gaming::DrawScreen()   //실제 화면에 스프라이트 및 이미지를 그리는 메서드
     
     //이미지 그리기
     bgImg->SetColorHide(0, 0, 255);
-    bgImg->Drawing(x, y, dir);
+    bgImg->Drawing(30, 50, 0);
     SDL_RenderPresent(renderer);    //화면 그리기
 }
 
