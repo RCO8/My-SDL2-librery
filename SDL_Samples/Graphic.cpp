777e932a4a5a07880171a04369f1ff37d3c38f2a
HyeokJae <gurwo63@naver.com>
2024-01-07 00:40:13 +0900

    윈도우 색상 마스크 구현 완료

--- SDL_Samples/Graphic.cpp
+++ SDL_Samples/Graphic.cpp
@@ -3,8 +3,8 @@
 Gradation::Gradation(SDL_Renderer* getRend) : renderer(getRend)
 {
 	nowGradientIndex = 2;
-	gradientColors[0] = { 0,0,0 };
-	gradientColors[1] = { 255,255,255 };
+	gradientColors[0] = { 0,0,0,255 };
+	gradientColors[1] = { 255,255,255,255 };
 }
 
 void Gradation::AddGradient(SDL_Color color)
@@ -62,9 +62,59 @@ void Gradation::DrawGradation(int x, int y, int w, int h)
 	gradientRect.w = w;
 	gradientRect.h = h;
 
-	//SDL_RenderCopy(sprRenderer, sprTexture, NULL, NULL);
+	Drawing();
 }
 void Gradation::DrawGradation(SDL_Rect rct)
 {
 	gradientRect = rct;
+
+	Drawing();
 }
+void Gradation::Drawing()
+{
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+	for (int i = 0; i < 256; i++)
+	{
+		SDL_SetRenderDrawColor(renderer,
+			gradientColors[0].r, gradientColors[0].g,
+			gradientColors[0].b, 10);
+		SDL_RenderFillRect(renderer, &gradientRect);
+	}
+}
+
+
+Mask::Mask(SDL_Renderer* rend) : renderer(rend) 
+{
+	//기본값 설정
+	defaultBlend = SDL_BLENDMODE_BLEND;
+	bgColor = { 255,255,255,128 };
+}
+void Mask::SetMaskColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+	bgColor.r = r;
+	bgColor.g = g;
+	bgColor.b = b;
+	bgColor.a = a;
+}
+void Mask::SetMaskColor(SDL_Color c) { bgColor = c; }
+
+void Mask::SetMaskMode(SDL_BlendMode blend) { defaultBlend = blend; }
+
+void Mask::DrawWindowMask(int x, int y, int w, int h)
+{
+	maskScreen.x = x;
+	maskScreen.y = y;
+	maskScreen.w = w;
+	maskScreen.h = h;
+}
+void Mask::DrawWindowMask(SDL_Rect rct) { maskScreen = rct; }
+
+void Mask::DrawMask()
+{
+	SDL_SetRenderDrawColor(renderer, bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+	SDL_SetRenderDrawBlendMode(renderer, defaultBlend);
+	SDL_RenderFillRect(renderer, &maskScreen);
+}
+
+void Mask::DrawFillMask() const { SDL_RenderFillRect(renderer, NULL); }
--- SDL_Samples/Graphic.h
+++ SDL_Samples/Graphic.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <SDL.h>
-#include <GL/GL.h>
+
 class Gradation
 {
 private:
@@ -13,7 +13,7 @@ private:
 	SDL_Rect gradientRect;
 public:
 	Gradation(SDL_Renderer* getRend);
-	~Gradation();
+	~Gradation() { SDL_DestroyRenderer(renderer); }
 
 	void AddGradient(SDL_Color color);
 	void RemoveGradient(int idx);
@@ -21,6 +21,8 @@ public:
 	void SetColorIndex(int idx, SDL_Color color);
 	void DrawGradation(int x, int y, int w, int h);
 	void DrawGradation(SDL_Rect rct);
+private:
+	void Drawing();	//public의 Draw함수의 공통사용
 };
 
 class Polygon
@@ -31,7 +33,23 @@ private:
 	int r;
 };
 
-class Mask
+class Mask	//창 색상 효과
 {
+private:
+	SDL_Renderer* renderer;
 
+	SDL_Color bgColor;	//마스킹 색상
+	SDL_BlendMode defaultBlend;	//마스킹 모드
+	SDL_Rect maskScreen;	//일부분 마스킹할 영역
+public:
+	Mask(SDL_Renderer* rend);
+	void SetMaskColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+	void SetMaskColor(SDL_Color c);
+	void SetMaskMode(SDL_BlendMode blend);
+
+	void DrawWindowMask(int x, int y, int w, int h);
+	void DrawWindowMask(SDL_Rect rct);
+	void DrawFillMask() const;
+private:
+	void DrawMask();	//이거는 설정받은대로 그리는 메서드 Draw~Mask를 호출 받을 때
 };
