fb756919b8285ef937b7e64c26ed453588bd2790
HyeokJae <gurwo63@naver.com>
2024-01-26 20:54:00 +0900

    그라데이션 제거

--- SDL_Samples/Graphic.h
+++ SDL_Samples/Graphic.h
@@ -1,30 +1,6 @@
 #pragma once
 #include <SDL.h>
 
-class Gradation
-{
-private:
-	SDL_Renderer* renderer;
-
-	const int maxColorIndex = 10;
-	SDL_Color gradientColors[10];
-	int nowGradientIndex;
-
-	SDL_Rect gradientRect;
-public:
-	Gradation(SDL_Renderer* getRend);
-	~Gradation() { SDL_DestroyRenderer(renderer); }
-
-	void AddGradient(SDL_Color color);
-	void RemoveGradient(int idx);
-	void RemoveGradient(SDL_Color color);
-	void SetColorIndex(int idx, SDL_Color color);
-	void DrawGradation(int x, int y, int w, int h);
-	void DrawGradation(SDL_Rect rct);
-private:
-	void Drawing();	//public의 Draw함수의 공통사용
-};
-
 class Mask	//창 색상 효과
 {
 private:
@@ -53,4 +29,9 @@ private:
 	SDL_Renderer* renderer;
 	int poly = 3;
 	int r;
+};
+
+class Rect
+{
+	
 };
